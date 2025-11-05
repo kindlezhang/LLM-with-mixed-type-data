2f8d797db51aeaa67f4f0b5b4327e4ca23742ea5
kindle zhang <kindle0118@163.com>
2025-11-05 13:15:10 -0500

    update

--- constants.py
+++ constants.py
@@ -10,16 +10,21 @@ data_base_dir = Path(data_base_dir)
 # MIMIC-CXR-JPG constants
 # ##################################
 
-mimic_cxr_data_dir = data_base_dir / "raw/physionet.org/files/mimic-cxr-jpg/2.1.0"
+mimic_cxr_data_dir = data_base_dir / "raw/mimic-cxr-jpg/2.1.0/files"
 # mimic_cxr_train_txt = mimic_cxr_data_dir / "train.txt"
 # mimic_cxr_valid_txt = mimic_cxr_data_dir / "valid.txt"
-mimic_cxr_chexpert_csv = mimic_cxr_data_dir / "mimic-cxr-2.1.0-chexpert.csv"
-mimic_cxr_meta_csv = mimic_cxr_data_dir / "mimic-cxr-2.1.0-metadata.csv"
-mimic_cxr_text_csv = mimic_cxr_data_dir / "mimic_cxr_sectioned.csv"
-mimic_cxr_split_csv = mimic_cxr_data_dir / "mimic-cxr-2.1.0-split.csv"
+mimic_cxr_chexpert_csv = mimic_cxr_data_dir / "mimic-cxr-2.0.0-chexpert.csv"
+mimic_cxr_meta_csv = mimic_cxr_data_dir / "mimic-cxr-2.0.0-metadata.csv"
+mimic_cxr_negbio_csv = mimic_cxr_data_dir / "mimic-cxr-2.0.0-negbio.csv"
+# mimic_cxr_text_csv = mimic_cxr_data_dir / "mimic_cxr_sectioned.csv"
+mimic_cxr_split_csv = mimic_cxr_data_dir / "mimic-cxr-2.0.0-split.csv"
 
 # Created csv
 mimic_cxr_train_csv = mimic_cxr_data_dir / "train.csv"
 mimic_cxr_valid_csv = mimic_cxr_data_dir / "valid.csv"
 mimic_cxr_test_csv =  mimic_cxr_data_dir / "test.csv"
+mimic_cxr_master_csv = mimic_cxr_data_dir / "master.csv"
+mimic_cxr_view_col = "ViewPosition"
+mimic_cxr_path_col = "Path"
+mimic_cxr_split_col = "split"
 
