b80ad3e684380290e0b4b987b05be64193802e80
kindle zhang <kindle0118@163.com>
2025-11-05 20:31:54 -0500

    split the mimic-cxr data

--- model/preprocess/constants.py
+++ model/preprocess/constants.py
@@ -10,18 +10,19 @@ data_base_dir = Path(data_base_dir)
 # MIMIC-CXR-JPG constants
 # ##################################
 
-mimic_cxr_data_dir = data_base_dir / "raw/mimic-cxr-jpg/2.1.0/files"
+mimic_cxr_data_dir = data_base_dir / "raw/mimic-cxr-jpg/2.1.0"
 # mimic_cxr_train_txt = mimic_cxr_data_dir / "train.txt"
 # mimic_cxr_valid_txt = mimic_cxr_data_dir / "valid.txt"
 mimic_cxr_chexpert_csv = mimic_cxr_data_dir / "mimic-cxr-2.0.0-chexpert.csv"
 mimic_cxr_meta_csv = mimic_cxr_data_dir / "mimic-cxr-2.0.0-metadata.csv"
 mimic_cxr_negbio_csv = mimic_cxr_data_dir / "mimic-cxr-2.0.0-negbio.csv"
-# mimic_cxr_text_csv = mimic_cxr_data_dir / "mimic_cxr_sectioned.csv"
+mimic_cxr_text_csv = mimic_cxr_data_dir / "mimic_cxr_sectioned.csv"
 mimic_cxr_split_csv = mimic_cxr_data_dir / "mimic-cxr-2.0.0-split.csv"
 
 # Created csv
 mimic_cxr_train_csv = mimic_cxr_data_dir / "train.csv"
-mimic_cxr_valid_csv = mimic_cxr_data_dir / "valid.csv"
+mimic_cxr_valid_csv = mimic_cxr_data_dir / "test.csv"
+# there is no valid set
 mimic_cxr_test_csv =  mimic_cxr_data_dir / "test.csv"
 mimic_cxr_master_csv = mimic_cxr_data_dir / "master.csv"
 mimic_cxr_view_col = "ViewPosition"
--- model/preprocess/mimic_cxr.py
+++ model/preprocess/mimic_cxr.py
@@ -1,8 +1,78 @@
 # This py file is used to make a classification for mimic data according to the table.
 
 import pandas as pd
-from model.constants import *
-from preprocess.utils import extract_mimic_text
+import  numpy as np
+from constants import *
+from utils import extract_mimic_text
 
-# extract_text = False
-# np.random.seed(2025)
+extract_text = False
+
+np.random.seed(2025)
+
+def main():
+    if extract_text:
+        extract_mimic_text()
+    metadata_df = pd.read_csv(mimic_cxr_meta_csv)
+    metadata_df = metadata_df[["dicom_id", "subject_id",
+                               "study_id", "ViewPosition"]].astype(str)
+    metadata_df["study_id"] = metadata_df["study_id"].apply(lambda x: "s"+x)
+    # Only keep frontal images
+    metadata_df = metadata_df[metadata_df["ViewPosition"].isin(["PA", "AP"])]
+
+    text_df = pd.read_csv(mimic_cxr_text_csv)
+    text_df.dropna(subset=["impression", "findings"], how="all", inplace=True)
+    text_df = text_df[["study", "impression", "findings"]]
+    text_df.rename(columns={"study": "study_id"}, inplace=True)
+
+    split_df = pd.read_csv(mimic_cxr_split_csv)
+    split_df = split_df.astype(str)
+    split_df["study_id"] = split_df["study_id"].apply(lambda x: "s"+x)
+    # TODO: merge validate and test into test.
+    split_df["split"] = split_df["split"].apply(
+        lambda x: "valid" if x == "validate" or x == "test" else x)
+
+    chexpert_df = pd.read_csv(mimic_cxr_chexpert_csv)
+    chexpert_df[["subject_id", "study_id"]] = chexpert_df[[
+        "subject_id", "study_id"]].astype(str)
+    chexpert_df["study_id"] = chexpert_df["study_id"].apply(lambda x: "s"+x)
+
+    master_df = pd.merge(metadata_df, text_df, on="study_id", how="left")
+    master_df = pd.merge(master_df, split_df, on=["dicom_id", "subject_id", "study_id"], how="inner")
+    master_df.dropna(subset=["impression", "findings"], how="all", inplace=True)
+    
+    n = len(master_df)
+    master_data = master_df.values
+
+    root_dir = str(mimic_cxr_data_dir).split("/")[-1] + "/files"
+    path_list = []
+    for i in range(n):
+        row = master_data[i]
+        file_path = "%s/p%s/p%s/%s/%s.jpg" % (root_dir, str(
+            row[1])[:2], str(row[1]), str(row[2]), str(row[0]))
+        path_list.append(file_path)
+        
+    master_df.insert(loc=0, column="Path", value=path_list)
+
+    # Create labeled data df
+    labeled_data_df = pd.merge(master_df, chexpert_df, on=[
+                               "subject_id", "study_id"], how="inner")
+    labeled_data_df.drop(["dicom_id", "subject_id", "study_id",
+                          "impression", "findings"], axis=1, inplace=True)
+
+    train_df = labeled_data_df.loc[labeled_data_df["split"] == "train"]
+    train_df.to_csv(mimic_cxr_train_csv, index=False)
+    test_df = labeled_data_df.loc[labeled_data_df["split"] == "valid"]
+    test_df.to_csv(mimic_cxr_test_csv, index=False)
+    # test_df = labeled_data_df.loc[labeled_data_df["split"] == "test"]
+    # test_df.to_csv(mimic_cxr_test_csv, index=False)
+
+    # master_df.drop(["dicom_id", "subject_id", "study_id"],
+    #                axis=1, inplace=True)
+
+    # Fill nan in text
+    master_df[["impression"]] = master_df[["impression"]].fillna(" ")
+    master_df[["findings"]] = master_df[["findings"]].fillna(" ")
+    master_df.to_csv(mimic_cxr_master_csv, index=False)
+
+if __name__ == "__main__":
+    main()
